1bb539e0c9b8c2460ec155ddee246ca63cb75c11
meisyakinata889-oss <meisyakinata889@gmail.com>
2026-09-13 18:08:04 +0700

    kondisional if else
    kode pemrograman temperature

---
+++ projecttemperature.py
@@ -0,0 +1,8 @@
+temperature = 31.0  
+
+if temperature < 16.0:
+    print("Turn the heater on")
+elif (temperature >= 16.0) and (temperature <= 30.0):
+    print("OK")
+else:
+    print("Turn the cooler on") 
